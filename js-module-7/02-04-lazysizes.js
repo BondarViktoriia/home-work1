9ecf5f1443ed8d8d42e61ba9542d8b9cd5f3e87c
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2022-09-11 16:48:50 +0300

    Update 02-04-lazysizes.js

--- js-module-7/02-04-lazysizes.js
+++ js-module-7/02-04-lazysizes.js
@@ -5,23 +5,13 @@
 
 if ('loading' in HTMLImageElement.prototype) {
     console.log('Браузер поддерживает lazyload');
-
-    const lazyImages = document.querySelectorAll('img[loading="lazy"]');
-
-    lazyImages.forEach(img => {
-        img.src = img.dataset.src;
-    })
+    addLazySizesScript();
+   
   
 } else {
     console.log('Браузер НЕ поддерживает lazyload');
     
-    const script = document.createElement('script');
-    script.src = "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
-    script.integrity = "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
-    script.crossOrigin = "anonymous";
-    script.referrerPolicy = "no-referrer"
-
-    document.body.appendChild(script);
+    addSrcAttrTolazyImages();
 }
 
 const lazyImages = document.querySelectorAll('img[loading="lazy"]');
@@ -33,4 +23,22 @@ lazyImages.forEach(image => {
 function onImageLoaded(evt) {
   console.log('Картинка загрузилась');
   evt.target.classList.add('appear');
+}
+
+function addLazySizesScript() {
+     const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+
+    lazyImages.forEach(img => {
+        img.src = img.dataset.src;
+    })
+}
+
+function addSrcAttrTolazyImages() {
+    const script = document.createElement('script');
+    script.src = "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+    script.integrity = "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
+    script.crossOrigin = "anonymous";
+    script.referrerPolicy = "no-referrer"
+
+    document.body.appendChild(script);
 }
